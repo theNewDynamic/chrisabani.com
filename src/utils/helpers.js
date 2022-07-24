80973b9ff5093af4eb66ea5df4686da42a31f2ba
Bud Parr <budparr@thenewdynamic.com>
2022-07-23 20:59:26 -0400

    Add slugify to helpers

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,33 +1,37 @@
 export function generateSlug(string) {
-  return string
-      .toString()
-      .trim()
-      .toLowerCase()
-      .replace(/\s+/g, '-')
-      .replace(/[^\w\-]+/g, '')
-      .replace(/\-\-+/g, '-')
-      .replace(/^-+/, '')
-      .replace(/-+$/, '');
+	return string
+		.toString()
+		.trim()
+		.toLowerCase()
+		.replace(/\s+/g, '-')
+		.replace(/[^\w\-]+/g, '')
+		.replace(/\-\-+/g, '-')
+		.replace(/^-+/, '')
+		.replace(/-+$/, '');
+}
+
+export function slugify(string) {
+	url => (url.toLowerCase().replace(/[^a-z0-9 -]/g, '').replace(/\s+/g, '-'))
 }
 
 export function generateCategoryData(categories) {
-  let categoryData = [];
-  categories.forEach((category) => {
-      categoryData.push({
-          name: category,
-          slug: `${generateSlug(category)}`,
-      });
-  });
-  return categoryData;
+	let categoryData = [];
+	categories.forEach((category) => {
+		categoryData.push({
+			name: category,
+			slug: `${generateSlug(category)}`,
+		});
+	});
+	return categoryData;
 }
 
 export function generateAuthorData(authors) {
-  let authorData = [];
-  authors.forEach((author) => {
-      authorData.push({
-          name: author,
-          slug: `${generateSlug(author)}`,
-      });
-  });
-  return authorData;
+	let authorData = [];
+	authors.forEach((author) => {
+		authorData.push({
+			name: author,
+			slug: `${generateSlug(author)}`,
+		});
+	});
+	return authorData;
 }
